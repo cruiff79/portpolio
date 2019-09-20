ee30d582813b10e973014a91593d30c32684bcda
cruiff79 <cruiff79@gmail.com>
2019-09-20 15:48:32 +0900

    Updates

---
+++ precache-manifest.f38c7fed94e3c450ae8fddab510cad77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493e1698d5d460a7085be788ee8eeef1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b82aff218a7d478de57d",
+    "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f9f7ed1c9519aad2c52c",
+    "url": "/portfolio/static/css/main.506f94b9.chunk.css"
+  },
+  {
+    "revision": "b82aff218a7d478de57d",
+    "url": "/portfolio/static/js/2.179bf6c6.chunk.js"
+  },
+  {
+    "revision": "f9f7ed1c9519aad2c52c",
+    "url": "/portfolio/static/js/main.c7e72f10.chunk.js"
+  },
+  {
+    "revision": "c98fad0fa5c777d28f1b",
+    "url": "/portfolio/static/js/runtime-main.36963274.js"
+  },
+  {
+    "revision": "ec4558329c6e918663c45dcda0c24f24",
+    "url": "/portfolio/static/media/background-keyboard.ec455832.jpg"
+  }
+]);
