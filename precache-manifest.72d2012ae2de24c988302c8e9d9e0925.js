76ff608943de82e114d3b419b1ed43d1019fc181
cruiff79 <cruiff79@gmail.com>
2020-02-11 15:10:13 -0600

    Updates

--- precache-manifest.72d2012ae2de24c988302c8e9d9e0925.js
+++ precache-manifest.72d2012ae2de24c988302c8e9d9e0925.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c4276d5190049ba3f81ca2408d8e3883",
+    "revision": "2e40de9012124070f896c971fc95ef58",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "b82aff218a7d478de57d",
+    "revision": "53eb25654ed53a46b85b",
     "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "9ee6ebaba177dbc981b8",
+    "revision": "a6fbfc0fdac0741cad6a",
     "url": "/portfolio/static/css/main.cc915893.chunk.css"
   },
   {
-    "revision": "b82aff218a7d478de57d",
-    "url": "/portfolio/static/js/2.179bf6c6.chunk.js"
+    "revision": "53eb25654ed53a46b85b",
+    "url": "/portfolio/static/js/2.8b31a225.chunk.js"
   },
   {
-    "revision": "9ee6ebaba177dbc981b8",
-    "url": "/portfolio/static/js/main.26ca12b7.chunk.js"
+    "revision": "a6fbfc0fdac0741cad6a",
+    "url": "/portfolio/static/js/main.64cee764.chunk.js"
   },
   {
     "revision": "c98fad0fa5c777d28f1b",
@@ -26,5 +26,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ec4558329c6e918663c45dcda0c24f24",
     "url": "/portfolio/static/media/background-keyboard.ec455832.jpg"
+  },
+  {
+    "revision": "d4a925d70fe6f5268ecb86bf4d9eec62",
+    "url": "/portfolio/static/media/main.d4a925d7.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.41b5ae8d9b015b7051fdc1c1f12ff773.js"
+  "/portfolio/precache-manifest.72d2012ae2de24c988302c8e9d9e0925.js"
 );
 
 self.addEventListener('message', (event) => {
