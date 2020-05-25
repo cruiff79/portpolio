6ce8963d999e318362cdcc7357e1b6d1132a5845
Jinyoung Park <cruiff79@gmail.com>
2020-05-25 10:57:53 -0600

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ class About extends React.Component {
                         <Col sm={9}>
                             <li>Over 4 years of experience as a web developer.</li>
                             <li>Familiar with Java and Spring Framework as a full stack developer.</li>
-                            <li>Graduated from Business Information Technology at Red River College.</li>
+                            <li>Graduated from Business Information Technology at Red River College in Canada.</li>
                             <li>Learned and studied C#, ASP .NET, PHP, React JS.</li>
                             <li>Love learning new IT skills and doing self-study.</li>
                         </Col>
