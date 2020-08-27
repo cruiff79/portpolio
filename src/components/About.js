8a3f3dfb9fa945d6d569980455b1154723198e3a
cruiff79 <cruiff79@gmail.com>
2020-08-26 20:09:39 -0700

    fix letters

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ class About extends React.Component {
                     <Row>
                         <Col sm={3}><h1>About Me</h1></Col>
                         <Col sm={9}>
-                            <li>About 4 years of experience as a web developer.</li>
+                            <li>Over 3 years of experience as a web developer.</li>
                             <li>Familiar with Java and Spring Framework as a full stack developer.</li>
                             <li>Graduated from Business Information Technology at Red River College in Canada.</li>
                             <li>Learned and studied C#, ASP .NET, PHP, React JS, Node JS.</li>
